6f86f40ff866875c5248a75c211ceac7f7c132d4
AdityaKaushalScrobits <107811034+AdityaKaushalScrobits@users.noreply.github.com>
2025-06-25 08:53:33 +0530

    make the product app
    application made

--- e_website/e_website/settings.py
+++ e_website/e_website/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'product' #custom app from product management
+    
 ]
 
 MIDDLEWARE = [
