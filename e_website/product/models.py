b1a829b7f2779e298b26228a42310e9a064d16cf
AdityaKaushalScrobits <107811034+AdityaKaushalScrobits@users.noreply.github.com>
2025-06-28 01:13:20 +0530

    last day scholar

--- e_website/product/models.py
+++ e_website/product/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name =models.CharField(max_length=100)
+    description = models.TextField()
+    price =  models.IntegerField()
+
+    #migration is used to create the table in the database
+
+
+wild photographic link
+couple wedding image link
+laptop image link
+image link
+
+crete only varchar feild
